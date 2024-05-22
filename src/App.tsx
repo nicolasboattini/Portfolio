7df8553827122e76fc41a890b8a90603ccf8ec84
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-05-22 20:18:16 -0300

    Add: Rutas y navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Home from "./pages/home/Home.tsx";
+import Projects from "./pages/projects/Projects.tsx";
+import Navbar from "./components/Navbar.tsx";
 
 function App() {
 
   return (
-    <>
-    
-    </>
+    <BrowserRouter>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
