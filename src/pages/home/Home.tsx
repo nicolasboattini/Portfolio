54839aad54bc8fa8d6ed4e2a4ec2d4983e6caf45
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-05-23 13:32:13 -0300

    Add: Section Skills

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <section className="w-full min-h-screen flex flex-col items-center p-2
+    <section className="w-full min-h-screen flex flex-col items-center p-2 gap-3
     bg-LIGHT-BG-P dark:bg-DARK-BG-P dark:text-D-T-P text-L-T-P">
       <article className="w-full sm:w-11/12 p-2 bg-LIGHT-BG-P-dark dark:bg-DARK-BG-P-dark">
         <p className="text-sm sm:text-xl text-L-T-S">
@@ -15,28 +15,25 @@ export default function Home() {
         </p>
 
         <div className="flex w-full">
-          <section className="
+          <section className="w-full flex flex-col gap-3 py-5 pl-[5vw]">
+            <section className="
             w-full
             flex flex-col justify-center items-center
-            sm:flex-row sm:justify-start sm:flex-wrap
-            gap-[5vw] py-5 pl-[5vw]"
-          >
-            <article className="w-48 h-48 sm:w-60 sm:h-60 rounded-full overflow-hidden border-2 border-solid border-LIGHT-D-P-dark dark:border-DARK-D-P-light">
-              <img className="w-full h-full object-cover object-center" src="FotoCVSinFondo.png" alt="Foto de perfil" />
-            </article>
+            sm:flex-row sm:justify-start gap-2"
+            >
+              <article className="w-48 h-48 sm:w-60 sm:h-60 rounded-full overflow-hidden border-2 border-solid border-LIGHT-D-P-dark dark:border-DARK-D-P-light">
+                <img className="w-full h-full object-cover object-center" src="FotoCVSinFondo.png" alt="Foto de perfil" />
+              </article>
 
-            <article className="sm:w-1/2 flex flex-col justify-center items-start gap-3 text-DARK-D-P text-lg sm:text-xl">
-              <p>👤 Nombre: <span className="text-L-T-S">Leandro Daniel Schugurensky</span></p>
-              <p>👨‍💻 Programador: <span className="text-L-T-S">🎓 Trainee</span></p>
-              <p>🎂 Edad: <span className="text-L-T-S">23 años</span></p>
-              <p>🌍 País: <span className="text-L-T-S">Argentina</span></p>
-              <p>📚 Educación: <span className="text-L-T-S">Estudiante autodidacta</span></p>
-              <p>🎯 Objetivo: <span className="text-L-T-S">Actualmente, estoy persiguiendo mi pasión por la programación con el objetivo de convertirme en un programador Front-end.</span></p>
-
-            </article>
-
-
-
+              <article className="sm:w-1/2 flex flex-col justify-center items-start gap-3 text-DARK-D-P text-lg sm:text-xl">
+                <p>👤 Nombre: <span className="text-L-T-S">Leandro Daniel Schugurensky</span></p>
+                <p>👨‍💻 Programador: <span className="text-L-T-S">🎓 Trainee</span></p>
+                <p>🎂 Edad: <span className="text-L-T-S">23 años</span></p>
+                <p>🌍 País: <span className="text-L-T-S">Argentina</span></p>
+                <p>📚 Educación: <span className="text-L-T-S">Estudiante autodidacta</span></p>
+                <p>🎯 Objetivo: <span className="text-L-T-S">Actualmente, estoy persiguiendo mi pasión por la programación con el objetivo de convertirme en un programador Front-end.</span></p>
+              </article>
+            </section>
           </section>
 
           <section className="min-h-full w-auto my-auto">
@@ -44,11 +41,58 @@ export default function Home() {
           </section>
         </div>
 
+        <p className="text-sm sm:text-xl text-L-T-S">
+          {`</`}
+          <span className="text-LIGHT-D-P dark:text-DARK-D-P-light">section</span>
+          {`>`}
+        </p>
+      </article>
 
+      <article className="w-full sm:w-11/12 p-2 bg-LIGHT-BG-P-dark dark:bg-DARK-BG-P-dark">
+        <p className="text-sm sm:text-xl text-L-T-S">
+          {`<`}
+          <span className="text-LIGHT-D-P dark:text-DARK-D-P-light">section</span>
+          {' '}
+          <span className="text-LIGHT-D-P-dark dark:text-DARK-D-P">id</span>
+          =
+          <span className="text-LIGHT-D-P dark:text-DARK-D-P-light">"section__skills"</span>
+          {`>`}
+        </p>
 
+        <div className="flex w-full">
+          <section className="w-full flex flex-col gap-3 py-5 pl-[5vw]">
+            <article className="
+            w-full
+            flex flex-col justify-center items-center
+            sm:flex-row sm:justify-start gap-2"
+            >
+              <p className="text-DARK-D-P text-xl">Skills</p>
+              <div className="w-full flex flex-col gap-2">
+                <p className="text-left w-full">Desarrollo Front-end:</p>
+                <section className="w-full flex flex-wrap gap-2 pl-4">
+                  <img src="https://img.shields.io/badge/HTML5%20-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white" alt="HTML5" />
+                  <img src="https://img.shields.io/badge/CSS%20-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white" alt="CSS3" />
+                  <img src="https://img.shields.io/badge/JavaScript%20-%23F7DF1E.svg?style=for-the-badge&logo=javascript&logoColor=black" alt="JavaScript" />
+                  <img src="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB" alt="React" />
+                  <img src="https://img.shields.io/badge/Tailwind_CSS-4392DB?style=for-the-badge&logo=tailwind-css&logoColor=white" alt="Tailwind" />
+                  <img src="https://img.shields.io/badge/Next.js-000000?style=for-the-badge&logo=next.js&logoColor=white" alt="Nextjs" />
+                </section>
+              </div>
 
+              <div className="w-full flex flex-col gap-2">
+                <p className="text-left w-full">Base de datos:</p>
+                <section className="w-full flex flex-wrap gap-2 pl-4">
+                  <img src="https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white" alt="mongodb" />
+                  <img src="https://img.shields.io/badge/Firebase-FFCA28?style=for-the-badge&logo=firebase&logoColor=black" alt="firebase" />
+                </section>
+              </div>
+            </article>
+          </section>
 
-
+          <section className="min-h-full w-auto my-auto">
+            <p className="inline-block text-8xl sm:text-9xl font-ubuntu font-normal text-DARK-D-P">;</p>
+          </section>
+        </div>
 
         <p className="text-sm sm:text-xl text-L-T-S">
           {`</`}
