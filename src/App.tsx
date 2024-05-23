aa300c17596034b05c1fdef18df5d1ddb1191f4d
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-05-22 21:23:05 -0300

    Add: Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Home from "./pages/home/Home.tsx";
 import Projects from "./pages/projects/Projects.tsx";
-import Navbar from "./components/Navbar.tsx";
+import Navbar from "./components/navbar/Navbar.tsx";
+import { ThemeProvider } from "./context/ThemeContext.tsx";
+
 
 function App() {
 
   return (
     <BrowserRouter>
-      <Navbar></Navbar>
-      <Routes>
-        <Route path="/" element={<Navigate to="/home" />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/projects' element={<Projects />} />
-      </Routes>
+      <ThemeProvider>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/projects' element={<Projects />} />
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
   )
 }
