e30be655e8763d70f9808f0d91c8b4a89166315e
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-05-27 16:53:14 -0300

    Refactor: Estructura y colores

--- src/components/TagsSection.tsx
+++ src/components/TagsSection.tsx
@@ -4,25 +4,35 @@ interface TagOpenProps {
 
 const TagOpen = ({ section }: TagOpenProps) => {
   return (
-    <p className="text-sm sm:text-xl text-L-T-S">
-      {`<`}
-      <span className="text-LIGHT-D-P dark:text-DARK-D-P-light">section</span>
-      {' '}
-      <span className="text-LIGHT-D-P-dark dark:text-DARK-D-P">id</span>
-      =
-      <span className="text-LIGHT-D-P dark:text-DARK-D-P-light">"section__{section}"</span>
-      {`>`}
-    </p>
+    <div className="flex flex-col text-sm sm:text-xl">
+      <p className="text-AMARILLO">
+        <span className="text-AZUL">const </span>
+        <span className="text-CREMA">{section}</span>
+        <span className="text-AZUL"> = </span>
+        {`()`}
+        <span className="text-AZUL">{` => `}</span>
+        {`{`}
+      </p>
+
+      <p className="indent-5 text-CREMA">
+        <span className="text-AZUL">return </span>
+        {`(`}
+      </p>
+    </div>
   )
-}
+};
 
 const TagClose = () => {
-  return(
-    <p className="text-sm sm:text-xl text-L-T-S">
-    {`</`}
-    <span className="text-LIGHT-D-P dark:text-DARK-D-P-light">section</span>
-    {`>`}
-  </p>
+  return (
+    <div className="flex flex-col text-sm sm:text-xl">
+      <p className="indent-5 text-CREMA">
+        {`)`}
+      </p>
+      <p className="text-AMARILLO">
+        {`}`}
+        <span className="text-AZUL">;</span>
+      </p>
+    </div>
   )
 }
 
