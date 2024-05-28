205d9eb2ae34b7d41cb547ca71dbf1b000e51d5d
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-05-28 20:37:54 -0300

    Add: Estructura del proyecto individual

--- src/pages/projects/Project.tsx
+++ src/pages/projects/Project.tsx
@@ -1,8 +1,9 @@
 import { useParams, Link } from 'react-router-dom';
+import Navbar from "../../components/navbar/Navbar.tsx";
 import { TagOpen, TagClose } from "../../components/TagsSection.tsx";
 import dataProjects from '../../data/dataProjects';
 import Techs from "../../components/Techs.tsx";
-
+import { Tooltip } from 'react-tooltip';
 
 export default function Project() {
   const { projectId } = useParams();
@@ -17,27 +18,66 @@ export default function Project() {
     <section className="w-full min-h-screen flex flex-col items-center 
     bg-LIGHT-BG-P dark:bg-DARK-BG dark:text-CREMA text-L-T-P">
 
-      <article className="w-full sm:w-11/12 p-2 bg-LIGHT-BG-P-dark dark:bg-DARK-BG-P-dark rounded-md shadow-md shadow-[#2c2c5d]">
-        <TagOpen section={project.title}/>
+      <Navbar />
 
-        <div className="flex w-full gap-2">
-          <section className="w-full flex flex-col gap-3 py-5 pl-[5vw]">
-            <section className="
-            w-full gap-[5vw]
-            grid grid-cols-1 sm:grid-cols-2"
+      <article className="mx-0 my-6 w-full h-auto md:h-auto md:w-11/12 md:pt-4 md:pr-4 md:pb-6 md:pl-7 p-2
+      bg-LIGHT-BG-P-dark dark:bg-DARK-BG-dark 
+        rounded-md border-2 border-DARK-BORDER-light"
+      >
+        <TagOpen section={`Proyectos["${project.title}"]`} />
+
+        <section className="flex w-full">
+          <article className="w-full flex flex-col gap-3 py-2 md:py-2 md:pl-[5vw]">
+            <div className="
+            w-full
+            flex flex-col justify-center items-center
+            md:flex-row md:justify-evenly md:items-center gap-2"
             >
               
 
-            </section>
-          </section>
 
-          <section className="min-h-full w-auto my-auto">
-            <p className="hidden sm:inline-block text-8xl sm:text-9xl font-ubuntu font-normal text-DARK-D-P">;</p>
-          </section>
-        </div>
+              <article className="w-full h-full flex gap-2 p-2"
+               >
+                
+                <div className="w-full flex flex-col gap-2">
+                  <p className="w-full text-center text-xl border-b-2 border-AMARILLO">{project.title}</p>
+
+                  <section className="w-full flex gap-2">
+                    <article className="w-11/12 flex flex-col gap-2 pl-2">
+                      <p className="indent-4">{project.description}</p>
+                    </article>
+
+                    <article className="w-1/12 flex flex-col items-center gap-2">
+                      <Link data-tooltip-id="my-tooltip" data-tooltip-content="Ver más" to={`/projects/${project.id}`}><span className="w-8 h-8 icon-[bi--plus-circle-fill] hover:text-AMARILLO"></span></Link>
+                      <a data-tooltip-id="my-tooltip" data-tooltip-content="Github" href={project.linkGithub} target="_blank"><span className="w-9 h-9 icon-[iconoir--github-circle] hover:text-AMARILLO"></span></a>
+                      <Tooltip id="my-tooltip" />
+                    </article>
+                  </section>
+
+                  <section className="w-full flex gap-2 flex-wrap justify-center">
+                    {project.techs.map(tech => (
+                      <Techs key={tech} tech={tech} />
+                    ))}
+                  </section>
+                </div>
+
+                <div className="w-full h-auto">
+                  <img className="w-full h-full object-cover object-center" src={`/projects/${project.id}/1.png`} alt="Portada del proyecto" />
+                </div>
+              </article>
+
+            </div>
+          </article>
+
+          <article className="min-h-full w-auto my-auto">
+            <p className="hidden md:inline-block text-8xl md:text-9xl font-ubuntu font-normal text-AZUL">;</p>
+          </article>
+
+        </section>
 
         <TagClose />
       </article>
+
     </section>
   );
 }
