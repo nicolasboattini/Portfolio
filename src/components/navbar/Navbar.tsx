7250225d207f227b11592dae47b942dda7924ada
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-05-28 19:26:23 -0300

    Refactor: Cambio de orden de objetos del navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,13 +6,15 @@ export default function Navbar() {
   const location = useLocation();
 
   return (
-    <section className="w-full h-12 bg-LIGHT-BG-S dark:bg-DARK-BG-S dark:text-D-T-P text-L-T-P p-2 flex items-center justify-between">
+    <section className="w-full h-12 p-2 flex items-center justify-between
+    bg-LIGHT-BG-S dark:bg-DARK-BG-dark dark:text-D-T-P text-L-T-P">
+      <p>{dataProfile.fullname}</p>
+
       <article className="flex gap-2">
         <Link to='/home' className={`border-b-2 border-transparent ${location.pathname === '/home' ? 'border-DARK-BORDER-light' : ''}`}>Home</Link>
         <Link to='/projects' className={`border-b-2 border-transparent ${location.pathname === '/projects' ? 'border-DARK-BORDER-light' : ''}`}>Projects</Link>
       </article>
 
-      <p>{dataProfile.fullname}</p>
 
       <ChangeTheme />
     </section>
