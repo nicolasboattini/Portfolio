0d9f53104b60065bc39b50966444942d4aa1ee82
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-05-30 20:34:50 -0300

    Refactor: Añadí un poco de padding

--- src/pages/projects/SelectResolution.tsx
+++ src/pages/projects/SelectResolution.tsx
@@ -11,7 +11,7 @@ export default function SelectResolution(props: SelectResolutionProps) {
 
   return (
     <section
-      className={`flex flex-col items-center justify-end p-1 
+      className={`flex flex-col items-center justify-end p-2 
       rounded border-2 
       hover:border-AMARILLO hover:bg-white hover:bg-opacity-10
       transition-colors duration-100 ease-in-out cursor-pointer select-none ${styleClass}`}
