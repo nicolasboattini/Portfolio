87c408ca7fc615d199dc6be2fc67851abe52046f
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-05-31 17:30:00 -0300

    Add: Componente de Link de secciones para ahorrar code

---
+++ src/components/navbar/LinkTo.tsx
@@ -0,0 +1,21 @@
+import { Link, useLocation } from "react-router-dom"
+
+interface LinkToProps {
+  to: string;
+  label: string;
+}
+
+export default function LinkTo({ to, label}: LinkToProps) {
+  const location = useLocation();
+  return (
+    <Link
+      to={to}
+      className={`border-b-2   
+          ${location.pathname === to
+          ? 'border-AZUL text-AZUL dark:border-CREMA dark:text-CREMA'
+          : 'border-LIGHT-GrisClaro text-LIGHT-GrisClaro  dark:border-DARK-BORDER-light '}
+          hover:border-AZUL hover:text-AZUL dark:hover:border-CREMA dark:hover:text-CREMA
+          transition-colors duration-300 ease-in-out
+    `}>{label}</Link>
+  )
+}
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,19 +1,19 @@
-import { Link, useLocation } from "react-router-dom"
 import ChangeTheme from "./ChangeTheme.tsx";
 import dataProfile from "../../data/dataProfile.ts";
+import LinkTo from "./LinkTo.tsx";
 
 export default function Navbar() {
-  const location = useLocation();
 
   return (
     <section className="w-full min-h-12 p-2 gap-2 flex flex-wrap items-center justify-center sm:justify-between 
-    bg-LIGHT-BG-S dark:bg-DARK-BG-dark dark:text-CREMA text-L-T-P">
+    bg-LIGHT-BG-dark dark:bg-DARK-BG-dark dark:text-CREMA text-LIGHT-T-dark">
       <p className="text-center">{dataProfile.fullname}</p>
 
-      <article className="flex flex-wrap justify-center gap-2 text-DARK-BORDER-light">
-        <Link to='/home' className={`border-b-2  border-DARK-BORDER-light ${location.pathname === '/home' ? 'border-CREMA dark:text-CREMA' : ''}`}>Home</Link>
-        <Link to='/projects' className={`border-b-2  border-DARK-BORDER-light ${location.pathname === '/projects' ? 'border-CREMA dark:text-CREMA' : ''}`}>Projects</Link>
-        <Link to='/cv' className={`border-b-2  border-DARK-BORDER-light ${location.pathname === '/cv' ? 'border-CREMA dark:text-CREMA' : ''}`}>Curriculum vitae</Link>
+      <article className="flex flex-wrap justify-center gap-2 
+        text-LIGHT-T dark:text-DARK-BORDER-light md:text-2xl select-none">
+          <LinkTo to="/home" label="Inicio" />
+          <LinkTo to="/projects" label="Proyectos" />
+          <LinkTo to="/cv" label="Curriculum vitae" />
       </article>
 
 
