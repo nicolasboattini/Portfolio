f2d569bae73d7c88d577cdc5326529c56e9f7371
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-05-31 01:17:00 -0300

    Add: Diseño Responsive para el navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,11 +6,11 @@ export default function Navbar() {
   const location = useLocation();
 
   return (
-    <section className="w-full h-12 p-2 flex items-center justify-between
+    <section className="w-full min-h-12 p-2 gap-2 flex flex-wrap items-center justify-center sm:justify-between 
     bg-LIGHT-BG-S dark:bg-DARK-BG-dark dark:text-CREMA text-L-T-P">
-      <p>{dataProfile.fullname}</p>
+      <p className="text-center">{dataProfile.fullname}</p>
 
-      <article className="flex gap-2 text-DARK-BORDER-light">
+      <article className="flex flex-wrap justify-center gap-2 text-DARK-BORDER-light">
         <Link to='/home' className={`border-b-2  border-DARK-BORDER-light ${location.pathname === '/home' ? 'border-CREMA dark:text-CREMA' : ''}`}>Home</Link>
         <Link to='/projects' className={`border-b-2  border-DARK-BORDER-light ${location.pathname === '/projects' ? 'border-CREMA dark:text-CREMA' : ''}`}>Projects</Link>
         <Link to='/cv' className={`border-b-2  border-DARK-BORDER-light ${location.pathname === '/cv' ? 'border-CREMA dark:text-CREMA' : ''}`}>Curriculum vitae</Link>
