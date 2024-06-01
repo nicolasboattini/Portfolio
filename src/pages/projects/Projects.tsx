930dc4b7b37633354ea9190c6f86c071a3e5e982
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-01 12:14:52 -0300

    Add: tabIndex para projects

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -25,34 +25,57 @@ export default function Projects() {
             w-full gap-[4vw]
             grid grid-cols-1 sm:grid-cols-2"
             >
-              {dataProjects.map(project => (
+              {dataProjects.map((project, index) => (
                 <article className="w-full h-full flex flex-col gap-2 p-2 rounded-lg
                  border-2 shadow
                  bg-AZUL bg-opacity-10  border-AZUL-dark shadow-AZUL-dark
                  dark:bg-white dark:bg-opacity-10 dark:border-AMARILLO dark:shadow-AMARILLO"
                   key={project.id}>
                   <div className="w-full h-auto">
-                    <img className="w-full h-full object-cover object-center rounded" src={`/projects/${project.id}/frontPage.png`} alt="Portada del proyecto" />
+                    <img tabIndex={parseInt(`${index + 1}01`)}
+                      className="w-full h-full object-cover object-center rounded" src={`/projects/${project.id}/frontPage.png`} alt="Portada del proyecto" />
                   </div>
 
                   <div className="w-full flex flex-col gap-2">
-                    <p className="w-full text-center text-xl border-b-2 border-AZUL-dark dark:border-AMARILLO">{project.title}</p>
+                    <p tabIndex={parseInt(`${index + 1}02`)}
+                      className="w-full text-center text-xl border-b-2 border-AZUL-dark dark:border-AMARILLO">{project.title}</p>
 
                     <section className="w-full flex gap-2">
                       <article className="w-11/12 flex flex-col gap-2 pl-2">
-                        <p className="indent-4">{project.description}</p>
+                        <p tabIndex={parseInt(`${index + 1}03`)} className="indent-4">{project.description}</p>
                       </article>
 
                       <article className="w-1/12 flex flex-col items-center gap-2">
-                        <Link data-tooltip-id="my-tooltip" data-tooltip-content="Ver más" to={`/projects/${project.id}`}><span className="w-8 h-8 icon-[bi--plus-circle-fill] hover:text-AZUL-dark dark:hover:text-AMARILLO"></span></Link>
-                        <a data-tooltip-id="my-tooltip" data-tooltip-content="Github" href={project.linkGithub} target="_blank"><span className="w-9 h-9 icon-[iconoir--github-circle] hover:text-AZUL-dark dark:hover:text-AMARILLO"></span></a>
+                        <Link
+                          data-tooltip-id="my-tooltip"
+                          data-tooltip-content="Ver más"
+                          to={`/projects/${project.id}`}
+                          tabIndex={parseInt(`${index + 1}04`)}
+                          aria-label="Ver más"
+                        >
+                          <span className="w-8 h-8 icon-[bi--plus-circle-fill] hover:text-AZUL-dark dark:hover:text-AMARILLO"></span>
+                        </Link>
+
+                        <a
+                          data-tooltip-id="my-tooltip"
+                          data-tooltip-content="Github"
+                          href={project.linkGithub} target="_blank"
+                          tabIndex={parseInt(`${index + 1}05`)}
+                          aria-label="Ver proyecto en github"
+                        >
+                          <span className="w-9 h-9 icon-[iconoir--github-circle] hover:text-AZUL-dark dark:hover:text-AMARILLO"></span>
+                        </a>
                         <Tooltip id="my-tooltip" />
                       </article>
                     </section>
 
                     <section className="w-full flex gap-2 flex-wrap justify-center">
                       {project.techs.map(tech => (
-                        <Techs key={tech} tech={tech} />
+                        <Techs
+                          key={tech}
+                          tech={tech}
+                          tabIndex={parseInt(`${index + 1}06`)}
+                        />
                       ))}
                     </section>
                   </div>
