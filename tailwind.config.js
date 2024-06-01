0433e460af1ad979ced40ceb71f374d236c6bb4c
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-01 09:45:51 -0300

    Drop: Estilos de la condif de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -60,61 +60,6 @@ const config = {
           GrisOscuro: '#1E1F24',
         },  
 
-
-
-
-        // Colores personalizados para el modo claro
-        'LIGHT-BG-P': {
-          light: '#f0f8ff',
-          DEFAULT: '#e0e7ff',
-          dark: '#c0c7ff'
-        },
-        'LIGHT-BG-S': {
-          light: '#dfefff',
-          DEFAULT: '#cfe7ff',
-          dark: '#afc7ff'
-        },
-        'LIGHT-D-P': {
-          light: '#ffedb5',
-          DEFAULT: '#ffdd85',
-          dark: '#ffcc55'
-        },
-        'L-T-P': {
-          light: '#1c1e26',
-          DEFAULT: '#030304',
-          dark: '#000000',
-        },
-        'L-T-S': {
-          light: '#ffffff',
-          DEFAULT: '#e4e4e4',
-          dark: '#b1b1b1',
-        },
-        // Colores personalizados para el modo oscuro
-        'DARK-BG-P': {
-          light: '#000046',
-          DEFAULT: '#000020',
-          dark: '#1a1a37',
-        },
-        'DARK-BG-S': {
-          light: '#0e2142',
-          DEFAULT: '#060e1c',
-          dark: '#03080f',
-        },
-        'DARK-D-P': {
-          light: '#ffcc50',
-          DEFAULT: '#ff9800',
-          dark: '#cc7900',
-        },
-        'D-T-P': {
-          light: '#fffbfe',
-          DEFAULT: '#f9f6f9',
-          dark: '#c5c3c5',
-        },
-        'D-T-S': {
-          light: '#85ffb2',
-          DEFAULT: '#84fca8',
-          dark: '#69c986',
-        },
       },
     },
   },
