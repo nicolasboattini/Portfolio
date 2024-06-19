f0691d3702e4d5d19f21737b2df9556704e83386
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-19 13:00:35 -0300

    Add: Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,16 @@
-
 export default function Footer() {
   return (
-    <div>footer</div>
+    <footer className="w-full min-h-10 p-2 gap-2 flex flex-wrap items-center justify-center
+    bg-LIGHT-BG-dark dark:bg-DARK-BG-dark dark:text-CREMA text-LIGHT-AzulOscuro">
+      <p>Template hecho por Leandro Daniel Schugurensky</p>
+      <a href="https://github.com/Schugu" target="_blank"
+        className="flex justify-center items-center hover:text-AZUL dark:hover:text-AMARILLO">
+        <span className="w-5 h-5 icon-[mdi--github]" />
+      </a>
+      <a href="https://www.linkedin.com/in/leandrodanielschugurensky/" target="_blank"
+        className="flex justify-center items-center hover:text-AZUL dark:hover:text-AMARILLO">
+        <span className="w-5 h-5 icon-[mdi--linkedin]" />
+      </a>
+    </footer>
   )
 }
