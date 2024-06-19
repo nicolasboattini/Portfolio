8ab7b1ef5606ed4c6d9105a5b1aa07f8d2c68caf
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-19 13:00:23 -0300

    Refactor: Text color

--- src/components/tooltip/Tooltip.tsx
+++ src/components/tooltip/Tooltip.tsx
@@ -17,7 +17,7 @@ const Tooltip = ({ text, children }: TooltipProps) => {
       {children}
       {visible && (
         <div className="absolute bottom-full px-2 py-1 mb-2 w-auto text-xs text-center rounded transform -translate-x-1/2 left-1/2
-        text-white bg-gray-800 border-2 dark:border-AMARILLO border-AZUL">
+        text-CREMA bg-gray-800 border-2 dark:border-AMARILLO border-AZUL">
           {text}
           <div className="absolute left-1/2 transform -translate-x-1/2 top-full w-0 h-0 
           border-l-4 border-l-transparent border-r-4 border-r-transparent border-t-4 
