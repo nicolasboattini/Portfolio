405af397441aa7238cd59c62e6d7fdec808d67bc
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-19 23:51:10 -0300

    Add: Text center

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="w-full min-h-10 p-2 gap-2 flex flex-wrap items-center justify-center
     bg-LIGHT-BG-dark dark:bg-DARK-BG-dark dark:text-CREMA text-LIGHT-AzulOscuro">
-      <p>Template hecho por Leandro Daniel Schugurensky</p>
+      <p className="text-center">Template hecho por Leandro Daniel Schugurensky</p>
       <a href="https://github.com/Schugu" target="_blank"
         className="flex justify-center items-center hover:text-AZUL dark:hover:text-AMARILLO">
         <span className="w-5 h-5 icon-[mdi--github]" />
