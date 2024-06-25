b4ad7d2fe2b14e4a6a88cfbbc8c3dba3d7bc460d
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-25 14:12:07 -0300

    Refactor: Dimenciones del home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -9,20 +9,20 @@ export default function Home() {
   return (
     <SectionCard sectionTitle="Presentación">
       <article className="w-full flex flex-col justify-center items-center
-                          md:flex-row md:justify-evenly md:items-center gap-2">
+                          md:flex-row md:justify-center md:items-center gap-[4vw]">
 
-        <div className="w-48 h-48 md:min-w-80 md:h-80 rounded-full overflow-hidden border-2 border-solid border-AZUL dark:border-AMARILLO
+        <div className="w-48 h-48 md:min-w-80 md:h-80 xl:min-w-96 xl:h-96  rounded-full overflow-hidden border-2 border-solid border-AZUL dark:border-AMARILLO
         bg-gradient-to-b  from-AZUL to-AMARILLO">
           <img className="w-full h-full object-cover object-center" src={dataProfile.profilePicture} alt="Foto de perfil" />
         </div>
 
-        <div className="md:w-full flex flex-col gap-5 md:gap-10">
-          <p className="text-center text-lg md:text-4xl text-AZUL dark:text-AMARILLO"
+        <div className="w-full md:w-1/2 flex flex-col items-center md:items-start gap-5 md:gap-10">
+          <p className="text-lg md:text-4xl text-AZUL dark:text-AMARILLO"
             tabIndex={7}
           >{dataProfile.fullname}
           </p>
 
-          <section className="w-full flex gap-5 flex-wrap justify-center items-center text-base md:text-2xl">
+          <section className="w-full flex gap-5 flex-wrap justify-center md:justify-start items-center text-base md:text-2xl">
             <Subtitle string={dataProfile.dev} tabIndex={8} />
             <Subtitle string={dataProfile.years} tabIndex={9} />
             <Tooltip text={dataProfile.nationality}>
@@ -32,7 +32,7 @@ export default function Home() {
 
 
 
-          <section className="flex flex-wrap gap-0.5 md:gap-4 justify-center items-center">
+          <section className="flex flex-wrap gap-0.5 md:gap-4 items-center md:items-start">
             {
               Object.entries(dataProfile.linksSocialNetworks).map(([network, link], index) => (
                 <Tooltip text={network}>
@@ -42,12 +42,12 @@ export default function Home() {
             }
           </section>
 
-          <section className="w-full flex flex-col gap-2 items-center">
+          <section className="w-full flex flex-col gap-2 items-center md:items-start">
             <p
-              className="text-LIGHT-AzulOscuro dark:text-AZUL text-base md:text-2xl"
+              className="text-LIGHT-AzulOscuro dark:text-AZUL text-base md:text-2xl text-center"
               tabIndex={50}
             >SKILLS</p>
-            <article className="flex flex-col items-start gap-4">
+            <article className="w-4/6 md:w-full flex flex-col items-start gap-4">
               <div className="flex flex-wrap gap-x-4 gap-y-1">
                 <p
                   className="text-AZUL-dark dark:text-AMARILLO text-md md:text-xl"
@@ -88,7 +88,7 @@ export default function Home() {
           </section>
 
           {
-            dataProfile.descript && <p className="text-base md:text-xl text-balance text-center">{dataProfile.descript}</p>
+            dataProfile.descript && <p className="text-base md:text-xl text-balance text-center md:text-left">{dataProfile.descript}</p>
           }
 
         </div>
