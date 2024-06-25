6b3647c0ef4ac218ee1b2540837cc3c97d8927a0
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-25 14:11:33 -0300

    Fix: dimenciones

--- src/components/SectionCard.tsx
+++ src/components/SectionCard.tsx
@@ -7,9 +7,9 @@ type SectionCardProps = {
 
 export default function SectionCard({ sectionTitle, children }: SectionCardProps) {
   return (
-    <section className="w-full min-h-screen flex flex-col items-center">
+    <section className="w-full flex flex-col items-center">
 
-      <article className="my-6 w-full h-auto md:h-auto md:w-11/12 md:pt-4 md:pr-4 md:pb-6 md:pl-7 p-2 
+      <article className="my-6 w-full md:w-11/12 md:pt-4 md:pr-4 md:pb-6 md:pl-7 p-2 
       rounded-md border-2 shadow 
       bg-LIGHT-BG-dark border-AZUL shadow-AZUL
       dark:bg-DARK-BG-dark dark:border-DARK-BORDER-light dark:shadow-DARK-BORDER-light">
