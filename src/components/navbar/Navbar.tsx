95c7645f47cf289fca25d0a5bad938f774d57917
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-27 11:07:20 -0300

    Add: Traducciones navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,17 @@
 import ChangeTheme from "./ChangeTheme.tsx";
-import dataProfile from "../../data/dataProfile.ts";
 import LinkTo from "./LinkTo.tsx";
 import Tooltip from "../../components/tooltip/Tooltip.tsx";
 import { useTranslation } from "react-i18next";
 import ChangeLanguage from "./ChangeLanguage.tsx";
+import { useProfile } from "../../context/ProfileContext.tsx";
 
 export default function Navbar() {
-  const [t] = useTranslation("global")
+  const { dataProfile } = useProfile();
+  const { t } = useTranslation(['global', 'profile', 'projects']);
+
+  if (!dataProfile) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <section className="w-full min-h-12 p-2 gap-2 flex flex-wrap items-center justify-center sm:justify-between 
@@ -15,23 +20,23 @@ export default function Navbar() {
 
       <article className="flex flex-wrap justify-center gap-2 
         text-LIGHT-AzulClaro dark:text-white md:text-2xl select-none">
-        <LinkTo to="/home" label={t("navbar.home")} tabIndex={2} />
+        <LinkTo to="/home" label={t("global:navbar.home")} tabIndex={2} />
         <p>/</p>
-        <LinkTo to="/projects" label="Proyectos" tabIndex={3} />
+        <LinkTo to="/projects" label={t("global:navbar.projects")} tabIndex={3} />
         <p>/</p>
-        <LinkTo to="/cv" label="Curriculum vitae" tabIndex={4} />
+        <LinkTo to="/cv" label={t("global:navbar.cv")} tabIndex={4} />
         <p>/</p>
-        <LinkTo to="/experience" label="Experiencia" tabIndex={5} />
+        <LinkTo to="/experience" label={t("global:navbar.experience")} tabIndex={5} />
         <p>/</p>
-        <LinkTo to="/education" label="Diplomas y certificados" tabIndex={6} />
+        <LinkTo to="/education" label={t("global:navbar.education")} tabIndex={6} />
         <p>/</p>
-        <LinkTo to="/contact" label="Contacto" tabIndex={7} />
+        <LinkTo to="/contact" label={t("global:navbar.contact")} tabIndex={7} />
       </article>
 
 
       <ChangeLanguage />
 
-      <Tooltip text="Cambiar tema" position="left">
+      <Tooltip text={t("global:navbar.changeTheme")} position="left">
         <ChangeTheme />
       </Tooltip>
     </section>
