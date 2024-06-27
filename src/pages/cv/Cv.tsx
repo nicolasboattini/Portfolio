5c522c0a8d9b36e655b1ef52f6d9e36a88c8f36a
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-27 14:21:48 -0300

    Add: Traducciones al cv

--- src/pages/cv/Cv.tsx
+++ src/pages/cv/Cv.tsx
@@ -1,12 +1,16 @@
 import SectionCard from "../../components/SectionCard.tsx";
-import dataProfile from "../../data/dataProfile.ts";
+import { useTranslation } from 'react-i18next';
+import { useProfile } from "../../context/ProfileContext.tsx";
 
-export default function Project() {
+export default function Cv() {
+  const { dataProfile } = useProfile();
+  const { t } = useTranslation(['global', 'profile', 'projects']);
+
   return (
-    <SectionCard sectionTitle="Curriculum vitae">
+    <SectionCard sectionTitle={t("global:cv.section")}>
       <article className="w-full h-full flex flex-col gap-2 p-2">
         <div className="pdf-container" style={{ width: '100%', height: '100vh' }}>
-          <embed src={dataProfile.linksSocialNetworks.CV} width="100%" height="100%" type="application/pdf" />
+          <embed src={dataProfile?.linksSocialNetworks.CV} width="100%" height="100%" type="application/pdf" />
         </div>
       </article>
     </SectionCard>
