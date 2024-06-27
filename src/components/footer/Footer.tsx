78d831a55393c84aca5aa6488eb6be3c43635c52
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-27 13:07:31 -0300

    Add: traducciones para el footer + tooltip

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,16 +1,25 @@
+import { useTranslation } from 'react-i18next';
+import Tooltip from "../../components/tooltip/Tooltip.tsx";
+
 export default function Footer() {
+  const { t } = useTranslation('global');
+
   return (
     <footer className="w-full min-h-10 p-2 gap-2 flex flex-wrap items-center justify-center
     bg-LIGHT-BG-dark dark:bg-DARK-BG-dark dark:text-CREMA text-LIGHT-AzulOscuro">
-      <p className="text-center">Template hecho por Leandro Daniel Schugurensky</p>
-      <a href="https://github.com/Schugu" target="_blank"
-        className="flex justify-center items-center hover:text-AZUL dark:hover:text-AMARILLO">
-        <span className="w-5 h-5 icon-[mdi--github]" />
-      </a>
-      <a href="https://www.linkedin.com/in/leandrodanielschugurensky/" target="_blank"
-        className="flex justify-center items-center hover:text-AZUL dark:hover:text-AMARILLO">
-        <span className="w-5 h-5 icon-[mdi--linkedin]" />
-      </a>
+      <p className="text-center">{t("global:footer.template")} Leandro Daniel Schugurensky</p>
+      <Tooltip text='Github'>
+        <a href="https://github.com/Schugu" target="_blank"
+          className="flex justify-center items-center hover:text-AZUL dark:hover:text-AMARILLO">
+          <span className="w-5 h-5 icon-[mdi--github]" />
+        </a>
+      </Tooltip>
+      <Tooltip text='Linkedin'>
+        <a href="https://www.linkedin.com/in/leandrodanielschugurensky/" target="_blank"
+          className="flex justify-center items-center hover:text-AZUL dark:hover:text-AMARILLO">
+          <span className="w-5 h-5 icon-[mdi--linkedin]" />
+        </a>
+      </Tooltip>
     </footer>
   )
 }
