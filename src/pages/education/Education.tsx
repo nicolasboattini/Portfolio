666cf95ec652afdd9d67dafde833d4b366e18994
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-07-01 15:23:21 -0300

    Add: Componente Loading y NotFound, y errores

--- src/pages/education/Education.tsx
+++ src/pages/education/Education.tsx
@@ -3,26 +3,31 @@ import { useProfile } from "../../context/ProfileContext.tsx";
 import { useTranslation } from 'react-i18next';
 
 export default function Education() {
-  const { dataProfile } = useProfile();
+  const { dataProfile, loading } = useProfile();
   const { t } = useTranslation('global');
 
   return (
     <SectionCard sectionTitle={t("global:education.section")}>
-      <article className="w-full gap-[4vw] grid grid-cols-2 sm:grid-cols-4">
-        {
-          dataProfile?.certificates?.map((certificate, index) => (
-            <a href={certificate} target="_blank"
-              className="w-full">
-              <img
-                className='transform  hover:scale-110 transition duration-300
+      {loading && <p>Cargando...</p>}
+      {!loading && !dataProfile && <p>No hay información disponible.</p>}
+
+      {!loading && dataProfile?.certificates &&
+        <article className="w-full gap-[4vw] grid grid-cols-2 sm:grid-cols-4">
+          {
+            dataProfile.certificates.map((certificate, index) => (
+              <a href={certificate} target="_blank"
+                className="w-full">
+                <img
+                  className='transform  hover:scale-110 transition duration-300
                 border-2 border-AMARILLO-light dark:border-AZUL rounded overflow-hidden'
-                src={`/profile/education/${index + 1}.jpg`}
-                alt={`Screenshot ${index + 1}`}
-              />
-            </a>
-          ))
-        }
-      </article>
+                  src={`/profile/education/${index + 1}.jpg`}
+                  alt={`Screenshot ${index + 1}`}
+                />
+              </a>
+            ))
+          }
+        </article>
+      }
     </SectionCard>
   )
 }
