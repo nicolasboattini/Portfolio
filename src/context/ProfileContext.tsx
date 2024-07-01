fbe1933a78388d17028ad045209e8dec646f452d
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-07-01 14:16:48 -0300

    Add: Opcionalidad de cada sección del home

--- src/context/ProfileContext.tsx
+++ src/context/ProfileContext.tsx
@@ -4,6 +4,7 @@ import { Profile, Project } from "./ProfileContextTypes";
 interface CombinedData {
   dataProfile: Profile | null;
   dataProjects: Project[] | null; 
+  loading: boolean;
 }
 
 const ProfileContext = createContext<CombinedData | null>(null);
@@ -21,26 +22,30 @@ interface ProfileProviderProps {
 }
 
 export const ProfileProvider: React.FC<ProfileProviderProps> = ({ children }) => {
-  const [combinedData, setCombinedData] = useState<CombinedData | null>(null);
+  const [combinedData, setCombinedData] = useState<CombinedData>({
+    dataProfile: null,
+    dataProjects: null,
+    loading: true,
+  });
 
   useEffect(() => {
     Promise.all([
       fetch('/src/translations/es/profile.json').then(response => response.json()),
       fetch('/src/translations/es/projects.json').then(response => response.json())
     ])
+      
       .then(([dataProfile, dataProjects]) => {
-        const combinedData: CombinedData = {
-          dataProfile,
-          dataProjects
-        };
-        setCombinedData(combinedData);
+        setTimeout(() => {
+          setCombinedData({
+            dataProfile,
+            dataProjects,
+            loading: false,
+          });
+        }, 2000);
       })
+
       .catch(error => console.error('Error fetching profile and project data:', error));
   }, []);
-
-  if (!combinedData) {
-    return <div>Loading...</div>;
-  }
 
   return (
     <ProfileContext.Provider value={combinedData}>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -9,109 +9,104 @@ import ContImg from "./ContImg.tsx";
 
 export default function Home() {
   const { t } = useTranslation(['global', 'profile', 'projects']);
-  const { dataProfile } = useProfile();
-
-  if (!dataProfile) {
-    return <div>Loading...</div>;
-  }
+  const { dataProfile, loading } = useProfile();
 
   return (
     <SectionCard sectionTitle={t("global:homePage.section")}>
-      <article className="w-full flex flex-col justify-center items-center
+      {loading && <p>Cargando...</p>}
+      {!loading && !dataProfile && <p>No hay información disponible</p>}
+
+      {!loading && dataProfile && (
+        <article className="w-full flex flex-col justify-center items-center
                           md:flex-row md:justify-center md:items-center gap-[4vw]">
 
-        <ContImg shape="circle">
-          <img className="w-full h-full object-cover object-center" src={dataProfile.profilePicture} alt="Foto de perfil" />
-        </ContImg>
-        
+          <ContImg shape="circle">
+            <img className="w-full h-full object-cover object-center" src={dataProfile.profilePicture} alt="Foto de perfil" />
+          </ContImg>
 
-        <div className="w-full md:w-1/2 flex flex-col items-center md:items-start gap-5 md:gap-10">
-          <p className="sm:text-left text-center text-lg md:text-4xl text-AZUL dark:text-AMARILLO"
-            tabIndex={7}
-          >
-            {dataProfile?.fullname}
-          </p>
+          <div className="w-full md:w-1/2 flex flex-col items-center md:items-start gap-5 md:gap-10">
+            <p className="sm:text-left text-center text-lg md:text-4xl text-AZUL dark:text-AMARILLO" tabIndex={7}>
+              {dataProfile.fullname}
+            </p>
 
-          <section className="w-full flex gap-5 flex-wrap justify-center md:justify-start items-center text-base md:text-2xl">
-            <Subtitle string={t("profile:dev")} tabIndex={8} />
-            <Subtitle string={t("profile:years")} tabIndex={9} />
-            <Tooltip text={dataProfile.nationality}>
-              <Flag nationality={dataProfile.nationality.toLowerCase()} tabIndex={10} />
-            </Tooltip>
-          </section>
+            <section className="w-full flex gap-5 flex-wrap justify-center md:justify-start items-center text-base md:text-2xl">
+              <Subtitle string={t("profile:dev")} tabIndex={8} />
+              <Subtitle string={t("profile:years")} tabIndex={9} />
+              <Tooltip text={dataProfile.nationality}>
+                <Flag nationality={dataProfile.nationality.toLowerCase()} tabIndex={10} />
+              </Tooltip>
+            </section>
 
-
-
-          <section className="flex flex-wrap gap-0.5 md:gap-4 items-center md:items-start">
-            {
-              Object.entries(dataProfile.linksSocialNetworks).map(([network, link], index) => (
-                <Tooltip text={network}>
-                  <SocialNetworkIcon key={network} socialNetwork={network} link={link} tabIndex={10 + index} />
-                </Tooltip>
-              ))
-            }
-          </section>
-
-          <section className="w-full flex flex-col gap-2 items-center md:items-start">
-            <p
-              className="text-LIGHT-AzulOscuro dark:text-AZUL text-base md:text-2xl text-center"
-              tabIndex={50}
-            >{t("global:homePage.skills")}</p>
-            <article className="w-4/6 md:w-full flex flex-col items-start gap-4">
-              <div className="flex flex-wrap gap-x-4 gap-y-1">
-                <p
-                  className="text-AZUL-dark dark:text-AMARILLO text-md md:text-xl"
-                  tabIndex={51}
-                >{t("global:homePage.frontEnd")}:</p>
-
-                {dataProfile.skills.frontEnd.map((tech, index) => (
-                  <Tooltip text={tech}>
-                    <IconosTechs key={tech} tech={tech} tabIndex={51 + index} />
+            {dataProfile.linksSocialNetworks &&
+              <section className="flex flex-wrap gap-0.5 md:gap-4 items-center md:items-start">
+                {Object.entries(dataProfile.linksSocialNetworks).map(([network, link], index) => (
+                  <Tooltip text={network} key={network}>
+                    <SocialNetworkIcon socialNetwork={network} link={link} tabIndex={10 + index} />
                   </Tooltip>
                 ))}
-              </div>
-
-              <div className="flex flex-wrap gap-x-4 gap-y-1">
-                <p
-                  className="text-AZUL-dark dark:text-AMARILLO text-md md:text-xl"
-                  tabIndex={100}
-                >{t("global:homePage.dataBase")}:</p>
-                {dataProfile.skills.dataBase.map((tech, index) => (
-                  <Tooltip text={tech}>
-                    <IconosTechs key={tech} tech={tech} tabIndex={101 + index} />
-                  </Tooltip>
-                ))}
-              </div>
-
-              <div className="flex flex-wrap gap-x-4 gap-y-1">
-                <p
-                  className="text-AZUL-dark dark:text-AMARILLO text-md md:text-xl"
-                  tabIndex={150}
-                >{t("global:homePage.versionControl")}:</p>
-                {dataProfile.skills.versionControl.map((tech, index) => (
-                  <Tooltip text={tech}>
-                    <IconosTechs key={tech} tech={tech} tabIndex={151 + index} />
-                  </Tooltip>
-                ))}
-              </div>
-            </article>
-          </section>
-
-          {
-            dataProfile.descript && <p className="text-base md:text-xl text-balance text-center md:text-left">{t("profile:descript")}</p>
-          }
-          <section className="w-full flex flex-col items-left gap-2">
-            {
-              dataProfile.hobbies && <p className="text-LIGHT-AzulOscuro dark:text-AZUL text-base md:text-2xl">{t("global:homePage.hobbies")}</p>
+              </section>
             }
-            {
-              dataProfile.hobbies && <p className="text-base md:text-xl text-balance text-center md:text-left">{t("profile:hobbies")}</p>
+
+            {dataProfile.skills &&
+              <section className="w-full flex flex-col gap-2 items-center md:items-start">
+                <p className="text-LIGHT-AzulOscuro dark:text-AZUL text-base md:text-2xl text-center" tabIndex={50}>
+                  {t("global:homePage.skills")}
+                </p>
+                <article className="w-4/6 md:w-full flex flex-col items-start gap-4">
+                  {dataProfile.skills.frontEnd &&
+                    <div className="flex flex-wrap gap-x-4 gap-y-1">
+                      <p className="text-AZUL-dark dark:text-AMARILLO text-md md:text-xl" tabIndex={51}>
+                        {t("global:homePage.frontEnd")}:
+                      </p>
+                      {dataProfile.skills.frontEnd.map((tech, index) => (
+                        <Tooltip text={tech} key={tech}>
+                          <IconosTechs tech={tech} tabIndex={51 + index} />
+                        </Tooltip>
+                      ))}
+                    </div>
+                  }
+
+                  {dataProfile.skills.dataBase &&
+                    <div className="flex flex-wrap gap-x-4 gap-y-1">
+                      <p className="text-AZUL-dark dark:text-AMARILLO text-md md:text-xl" tabIndex={100}>
+                        {t("global:homePage.dataBase")}:
+                      </p>
+                      {dataProfile.skills.dataBase.map((tech, index) => (
+                        <Tooltip text={tech} key={tech}>
+                          <IconosTechs tech={tech} tabIndex={101 + index} />
+                        </Tooltip>
+                      ))}
+                    </div>
+                  }
+
+                  {dataProfile.skills.versionControl &&
+                    <div className="flex flex-wrap gap-x-4 gap-y-1">
+                      <p className="text-AZUL-dark dark:text-AMARILLO text-md md:text-xl" tabIndex={150}>
+                        {t("global:homePage.versionControl")}:
+                      </p>
+                      {dataProfile.skills.versionControl.map((tech, index) => (
+                        <Tooltip text={tech} key={tech}>
+                          <IconosTechs tech={tech} tabIndex={151 + index} />
+                        </Tooltip>
+                      ))}
+                    </div>
+                  }
+                </article>
+              </section>
             }
-          </section>
 
-        </div>
+            {dataProfile.descript && <p className="text-base md:text-xl text-balance text-center md:text-left">{t("profile:descript")}</p>}
 
-      </article>
+            {dataProfile.hobbies &&
+              <section className="w-full flex flex-col items-left gap-2">
+                <p className="text-LIGHT-AzulOscuro dark:text-AZUL text-base md:text-2xl">{t("global:homePage.hobbies")}</p>
+                <p className="text-base md:text-xl text-balance text-center md:text-left">{t("profile:hobbies")}</p>
+              </section>
+            }
+          </div>
+
+        </article>
+      )}
     </SectionCard>
-  )
+  );
 }
