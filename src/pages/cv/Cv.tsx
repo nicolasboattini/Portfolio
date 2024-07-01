3b41f3f6723758b44bf5530e91335491bc751426
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-07-01 15:23:31 -0300

    Add: Componente Loading y NotFound, y errores

--- src/pages/cv/Cv.tsx
+++ src/pages/cv/Cv.tsx
@@ -3,19 +3,21 @@ import { useTranslation } from 'react-i18next';
 import { useProfile } from "../../context/ProfileContext.tsx";
 
 export default function Cv() {
-  const { dataProfile } = useProfile();
+  const { dataProfile, loading } = useProfile();
   const { t } = useTranslation(['global', 'profile', 'projects']);
 
   return (
     <SectionCard sectionTitle={t("global:cv.section")}>
-      <article className="w-full h-full flex flex-col gap-2 p-2">
-        {
-          dataProfile?.linksSocialNetworks?.CV &&
+      {loading && <p>Cargando...</p>}
+      {!loading && !dataProfile && <p>No hay información disponible.</p>}
+
+      {!loading && dataProfile?.linksSocialNetworks?.CV &&
+        <article className="w-full h-full flex flex-col gap-2 p-2">
           <div className="pdf-container" style={{ width: '100%', height: '100vh' }}>
             <embed src={t("profile:linksSocialNetworks.CV")} width="100%" height="100%" type="application/pdf" />
           </div>
-        }
-      </article>
+        </article>
+      }
     </SectionCard>
   );
 }
