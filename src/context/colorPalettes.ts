8fbac00f7a808288fbb5317777784f678f45e80f
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-07-04 00:18:37 -0300

    Refactor: Sistema de cambio de colores por variables

---
+++ src/context/colorPalettes.ts
@@ -0,0 +1,58 @@
+export const darkThemeColors = {
+  primary: {
+    light: '#cca340',
+    DEFAULT: "#ffcc50",
+    dark: "#cca340",
+  },
+  secondary: {
+    light: '#89c4f3',
+    DEFAULT: '#2196f3',
+    dark: '#145c96',
+  },
+  background: {
+    DEFAULT: "#060e1c",
+    light: "#0e192b"
+  },
+  text: {
+    DEFAULT: '#f1e1cf',
+    dark: '#ab9f92',
+  },
+  border: {
+    light: '#637591',
+    DEFAULT: '#50617C',
+    dark: '#3F4F6A',
+  },
+  accent1: '#18263E',
+  accent2: '#060e1c',
+  accent3: '#637591',
+};
+
+export const lightThemeColors = {
+  primary: {
+    light: '#89c4f3',
+    DEFAULT: '#2196f3',
+    dark: '#145c96',
+  },
+  secondary: {
+    light: '#ffdd8b', // Ajusta el color según tus necesidades
+    DEFAULT: '#145c96',
+    dark: '#cca340',
+  },
+  background: {
+    light: '#FAFCFF',
+    DEFAULT: '#F5FAFF',
+  },
+  text: {
+    light: '#55657E',
+    DEFAULT: '#55657E', 
+    dark: '#16202F',
+  },
+  border: {
+    light: '#637591',
+    DEFAULT: '#50617C',
+    dark: '#3F4F6A',
+  },
+  accent1: '#18263E',
+  accent2: '#060e1c',
+  accent3: '#637591',
+};
